6602527fa392ee98c19e1e0e9b9b82930cae0e24
Avadhoot Pawar <avadhootpawar.ap@gmail.com>
2023-07-28 19:40:55 +0530

    MyTodoApp

--- my-app/src/components/addTask/Taskinput.tsx
+++ my-app/src/components/addTask/Taskinput.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
-
-
-
+import React, { useState } from 'react'
+import TaskList from '../todoList/TaskList';
 function Taskinput() {
+
+  const [task,setTask]=useState<string []>([])
+  const [inputtask,setInputTask]=useState("");
+
+  function handleInput(e){
+    setInputTask(e.target.value);
+  }
+
+  function addtask(){
+    setTask((task)=>[...task,inputtask]);
+    
+  }
+
   return (
     <div>
       <label >Task:</label><br></br>
-      <input type="text" id="task" name="task" /><br></br>
-      <button type='button' id='btn'>ADD</button>
+      <input type="text" id="task" name="task" onChange={handleInput}/><br></br>
+      <p>{task}</p>
+      <button type='button' id='btn' onClick={addtask}>ADD</button>
+      <TaskList></TaskList>
     </div>
   )
 }
